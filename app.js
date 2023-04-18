487ede3e7d619f2b46cfd3da56f1d764502579a7
blksy <79380937+blksy@users.noreply.github.com>
2023-04-18 16:21:32 +0200

    added vue directives in index.html to bind the button and divs with methods and computer properties

--- app.js
+++ app.js
@@ -10,13 +10,21 @@ const app = Vue.createApp({
             //random dmg between 5 and 15 points
          const attackPower = Math.floor(Math.random() * (15 - 5) + 5);
          this.monsterHealth -= attackPower;
-         this.attackPlayer();
+         this.attackPlayer(); 
         },
         attackPlayer(){
          const attackPower = Math.floor(Math.random() * (19 - 8) + 8); 
          this.playerHealth -= attackPower; 
         }
-    }
+    },
+    computed:{
+      monsterBarStyle(){
+        return {width: this.monsterHealth + '%'};
+      },
+      playerBarStyle(){
+        return {width: this.playerHealth + '%'}
+      }
+    },
 })
 
 app.mount("#game")
