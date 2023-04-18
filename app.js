574bacef9a8260894fce111cfafca23b5d4a76e7
blksy <79380937+blksy@users.noreply.github.com>
2023-04-18 16:58:20 +0200

    added a heavyAttack method which player can use every three rounds

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const app = Vue.createApp({
     data(){
         return{
           playerHealth: 100,
-          monsterHealth: 100
+          monsterHealth: 100,
+          noOfRounds: 0
         };
     },
     methods:{
         attackMonster(){
+         this.noOfRounds++;
             //random dmg between 5 and 15 points
          const attackPower = Math.floor(Math.random() * (15 - 5) + 5);
          this.monsterHealth -= attackPower;
@@ -15,7 +17,14 @@ const app = Vue.createApp({
         attackPlayer(){
          const attackPower = Math.floor(Math.random() * (19 - 8) + 8); 
          this.playerHealth -= attackPower; 
-        }
+        },
+        heavyAttackMonster(){
+        this.noOfRounds++;
+        const attackPower = Math.floor(Math.random() * (25 - 10) + 10); 
+        this.monsterHealth -= attackPower;
+        this.attackPlayer();
+        },
+
     },
     computed:{
       monsterBarStyle(){
